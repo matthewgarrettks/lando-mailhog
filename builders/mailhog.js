c7d1221780a2766960304296745beda5b7ab4b00
Matt Garrett <86023244+matthewgarrettks@users.noreply.github.com>
2024-04-09 10:35:45 -0500

    most tests passing

--- builders/mailhog.js
+++ builders/mailhog.js
@@ -4,18 +4,12 @@
 const _ = require('lodash');
 const path = require('path');
 
-// Cmdz
-const amd64dlUrl = 'https://github.com/mailhog/mhsendmail/releases/download/v0.2.0/mhsendmail_linux_amd64';
-const arm64dlUrl = 'github.com/evertiro/mhsendmail/releases/download/v0.2.0-M1/mhsendmail_linux_arm64';
-const downloadCmd = (url = amd64dlUrl) => `curl -fsSL -o /usr/local/bin/mhsendmail ${url}`;
-const chmodCmd = 'chmod +x /usr/local/bin/mhsendmail';
-
-// Builder
+// Exports for lando install file
 module.exports = {
   name: 'mailhog',
   config: {
-    version: 'v1.0.1',
-    supported: ['v1.0.0', 'v1.0.1'],
+    version: 'v1.0.2',
+    supported: ['v1.0.0', 'v1.0.1', 'v1.0.2'],
     confSrc: path.resolve(__dirname, '..', 'config'),
     hogfrom: [],
     port: '1025',
@@ -24,8 +18,6 @@ module.exports = {
   parent: '_service',
   builder: (parent, config) => class LandoMailHog extends parent {
     constructor(id, options = {}) {
-      // Are we strapped?
-      const isArmed = _.get(options, '_app._config.isArmed', false);
       // Merge user opts on top of defaults
       options = _.merge({}, config, options);
       // Build the default stuff here
@@ -50,22 +42,22 @@ module.exports = {
           },
         },
       };
-      // Change the me user
+      // Change the "me" user
       options.meUser = 'mailhog';
       // Add in hogfrom info
       options.info = {hogfrom: options.hogfrom};
       // Mailhog needs to do some crazy shit on other services to work
-       _.forEach(options.hogfrom, hog => {
+      _.forEach(options.hogfrom, hog => {
         // Get download URL based on ARCH
-        const mhsendmailURL = (isArmed) ? arm64dlUrl : amd64dlUrl;
+        // const mhsendmailURL = (isArmed) ? arm64dlUrl : amd64dlUrl;
         // Add some build tazk
-        const dlc = downloadCmd(mhsendmailURL);
-        require('../utils/add-build-step')([dlc, chmodCmd], options._app, hog, 'build_as_root_internal');
+        const dlc = '/helpers/mailhog.sh';
+        require('../utils/add-build-step')([dlc], options._app, hog, 'build_as_root_internal');
         // Set the hogfrom with some extra things
         options.sources.push({services: _.set({}, hog, {
-          environment: {MH_SENDMAIL_SMTP_ADDR: 'sendmailhog:1025'},
-          volumes: [`${options.confDest}/mailhog.ini:/usr/local/etc/php/conf.d/zzzz-lando-mailhog.ini`],
-        })});
+            environment: {MH_SENDMAIL_SMTP_ADDR: 'sendmailhog:1025'},
+            volumes: [`${options.confDest}/mailhog.ini:/usr/local/etc/php/conf.d/zzzz-lando-mailhog.ini`],
+          })});
       });
 
       // Set the mailhpog
